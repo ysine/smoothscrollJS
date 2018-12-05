1b4b2660e99445152d00d4083bf19e1742644b8d
Yasien <kingysine@gmail.com>
2018-12-05 15:00:35 +0100

    New update

--- js/horPage.js
+++ js/horPage.js
@@ -9,10 +9,13 @@ deLinkjes.forEach( (item) => {
   });
   alleLinkjesArr.push(item);
 });
-
+const pijlLinks = document.querySelector('.nav__pijl--links');
+const pijlRechts = document.querySelector('.nav__pijl--rechts');
 const activeren = (num) => {
   // verwijder eventuele actieve statussen van de linkjes
   verwijderActief();
+  // toon of verwijder pijltjes
+  toonVerbergpijlen();
   // activeer de link nul
   alleLinkjesArr[num].classList.add('nav--actief');
   // schuif inhoud nut voor
@@ -24,5 +27,53 @@ const verwijderActief = () => {
     item.classList.remove('nav--actief');
   })
 }
+
+// script voor pijltjes
+// functie om de volgende setion te activeren
+const volgende = () => {
+  if(index<alleLinkjesArr.length-1){
+    index++;
+  } else{
+    index = 0;
+  }
+  activeren(index);
+}
+
+const vorige = () => {
+  if(index>0){
+    index--;
+  } else{
+    // de index mag nooit onder de 0 komen
+    index = alleLinkjesArr.length-1;
+  }
+  activeren(index);
+}
+
+const toonVerbergpijlen = () =>{
+  if( index == 0 ) {
+    pijlLinks.style.display = "none";
+  } else {pijlLinks.style.display = "block";}
+
+  if (index == alleLinkjesArr.length -1) {
+    pijlRechts.style.display = "none";
+  } else {pijlRechts.style.display = "block";}
+}
+
+pijlRechts.addEventListener('click', volgende);
+pijlLinks.addEventListener('click', vorige);
+
+// toetsenbord events toevoegen
+document.addEventListener('keyup', (e) =>{
+  if( e.keyCode == 39 || e.keyCode == 32){
+    volgende();
+  }
+  if (e.keyCode == 37){
+    vorige();
+  }
+})
+// swipe events toevoegen met pureSwipe library
+document.addEventListener('swiped-left', volgende);
+document.addEventListener('swiped-right', vorige);
+
 // bij de start eerste item
 activeren(index);
